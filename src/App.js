8fbfaee082379e6ccabe52bc74932d5a8db62f9f
avramstefan <94166855+avramstefan@users.noreply.github.com>
2023-07-09 01:33:45 +0300

    Added cart and triggering it with click on cart icon and closing it with click outside or click on x, solved the twice click icon problem.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,37 @@ import Admin from "./pages/Admin";
 import AdminAddService from "./pages/AdminAddService";
 import Product from "./pages/Product";
 import Notfound from "./pages/404";
+import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import ProductDetails from "./components/ProductDetails";
-import Cart from "./pages/Cart";
+import CartPanel from "./components/CartPanel";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
+
 
 const App = () => {
+  const navigate = useNavigate();
+
+  // const handleCart = () => {
+  //   history.push('/cartPanel');
+  // };
+
   return (
     <section className="">
       <Navbar />
+
+      {/* <div className="flex  justify-between pr-7 pl-2 gap-10 ">
+          <button
+            onClick={() => navigate("cartPanel")}
+            className="backdrop-blur-xl bg-black bg-opacity-50 text-sky-50 px-4 py-7"
+          >
+            <span>
+              <BsArrowLeft />
+            </span>
+          </button>
+        </div> */}
 
       <main>
         <Routes>
@@ -28,6 +49,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/cartPanel" element={<CartPanel />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/adminAddService" element={<AdminAddService />} />
           <Route path="*" element={<Notfound />} />
