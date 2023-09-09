9954dbfe11a48ead7cc137f3ec9fc9bc696865f0
avramstefan <94166855+avramstefan@users.noreply.github.com>
2023-09-09 19:33:59 +0300

    [WIP] Payments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
+import React from 'react';
+
 const Footer = () => {
   return (
-    <div className="bg-gray-200 py-5 text-gray-900 text-center">
+    <>
+    <div className="flex-grow"></div>
+    <div className="bg-gray-200 py-5 text-gray-900 text-center w-full  bottom-0">
       <p>Copyright © 2023 SilverCare. Powered by SilverCare.</p>
     </div>
+  </>
   );
 };
 
