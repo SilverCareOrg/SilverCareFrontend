86f275119175cda2bf048e9575927a61f778171b
avramstefan <94166855+avramstefan@users.noreply.github.com>
2023-10-22 15:07:48 +0300

    Home search responsive

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -15,21 +15,20 @@ const HomeSearch = () => {
         setSearchTerm(e.target.value);
     };
     
-
   return (
-    <div className="flex flex-col items-center justify-start py-[3.5rem] px-[0rem] box-border text-center text-[2.5rem]">
-      <div className="w-[77rem] flex flex-col items-center justify-center">
-        <div className="self-stretch flex flex-col items-center justify-start gap-[2.5rem]">
-          <div className="flex flex-col items-start justify-start gap-[1.5rem]">
-            <div className="relative tracking-[0.12em] leading-[120%] font-semibold flex items-center justify-center w-[57rem] h-[2.5rem] shrink-0">
+    <div className="flex flex-col items-center justify-start max-lg:py-[2rem] lg:py-[3.5rem] px-[0rem] box-border text-center max-lg:text-[1.88rem] lg:text-[2.5rem]">
+      <div className="max-lg:w-[20rem] max-lg:h-[15rem] lg:w-[77rem] flex flex-col items-center justify-center">
+        <div className="self-stretch flex flex-col items-center justify-center max-lg:gap-[1.5rem] lg:gap-[2.5rem]">
+          <div className="max-lg:self-stretch flex flex-col items-start justify-start max-lg:gap-[1rem] lg:gap-[1.5rem]">
+            <div className="max-lg:self-stretch relative tracking-[0.12em] leading-[120%] font-semibold lg:w-[57rem] lg:h-[2.5rem]">
               Redescoperă magia vieții!
             </div>
-            <div className="relative text-[1.5rem] tracking-[0.1em] leading-[120%] font-semibold flex items-center justify-center w-[57rem]">
+            <div className="relative max-lg:text-[1.13rem] lg:text-[1.5rem] tracking-[0.1em] leading-[120%] max-lg:font-medium lg:font-semibold flex items-center justify-center lg:w-[57rem]">
               Activități și experiențe pentru vârstnici
             </div>
           </div>
-          <div className="self-stretch flex flex-col items-center justify-end py-[0rem] px-[13.13rem] text-left text-[1rem] text-text-fields-grey-hf">
-            <div className="self-stretch rounded-lg bg-white box-border h-[3.5rem] flex flex-row items-center justify-start py-[0rem] pr-[1.5rem] pl-[1rem] gap-[1rem] border-[1.5px] border-solid border-text-fields-grey-hf">
+          <div className="self-stretch flex flex-col items-center justify-end lg:px-[13.13rem] text-left max-lg:text-[0.75rem] lg:text-[1rem] text-text-fields-grey-hf">
+            <div className="self-stretch rounded-lg bg-white box-border max-lg:h-[3rem] lg:h-[3.5rem] flex flex-row items-center justify-start py-[0rem] pr-[1.5rem] pl-[1rem] gap-[1rem] border-[1.5px] border-solid border-text-fields-grey-hf">
                 <div className="flex-1 relative tracking-[0.08em] leading-[120%] flex items-center h-[2rem]">
                 <input
                     type="text"
