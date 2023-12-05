6592090e8c010eecaf6de4b311db5e0cc3099f93
Ion Stefan <ion.adrian.stefann@gmail.com>
2023-12-05 13:40:23 +0100

    Implemented full flow and functionality of the cart

--- src/components/PaymentCartPanel.js
+++ src/components/PaymentCartPanel.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import back_icon_arrow from "../styles/icons/back_icon_arrow.svg";
 import ProgressBar from "./ProgressBar";
 import ProductsList from "./ProductList";
+import CheckoutDetails from "./CheckoutDetails";
 
 const PaymentCartPanel = ({}) => {
   const [price_total, setPriceTotal] = useState(0);
@@ -116,86 +117,175 @@ const PaymentCartPanel = ({}) => {
     }
   };
 
+  const [cartPage, setCartPage] = useState("1");
+  const continueRequirements = termsAndConditionsChecked && acceptTermsChecked;
   return (
     <>
-      <ProgressBar cartStep={"1"} />
-      <div className="w-[wh] flex flex-col bg-white overflow-hidden text-left text-[1rem] text-dark-navy font-text-body">
-        <div className="w-[wh] max-w-[1050px] flex justify-center items-center xl:mr-[36rem] lg:mr-[22rem] md:mr-[9rem] sm:ml-[0rem] ">
-          <div className=" flex flex-col items-center sm:items-start py-[1.5rem] gap-[2.5rem]">
-            <div className="flex">
-              <button
-                className="self-stretch flex flex-row  gap-[1rem]"
-                onClick={() => (window.location.href = "/product")}
-              >
-                <img
-                  className="relative rounded-sm w-[0.83rem] h-[1.63rem]"
-                  alt=""
-                  src={back_icon_arrow}
-                />
-                <div className="relative tracking-[0.05em] leading-[1.5rem] font-medium flex items-center h-[1.5rem] shrink-0">
-                  Continua cumparaturile
-                </div>
-              </button>
-            </div>
-            <div className="flex flex-col gap-[1.5rem] text-text-fields-grey-hf w-[wh]">
-              <ProductsList
-                handleRemoveButton={handleRemoveButton}
-                products={products}
-                baseUrl={baseUrl}
-              />
-
-              <div className=" tracking-[0.08em] leading-[120%] font-bold text-[1.2rem] flex gap-[0.5rem] text-dark-navy">
-                Total de plată: {price_total} RON
-              </div>
-
-              <div className="flex text-dark-navy">
-                <div className="flex items-center justify-center">
-                  <input
-                    type="checkbox"
-                    id="acceptTermsCheckbox"
-                    className="mr-2 cursor-pointer h-5 w-5"
-                    checked={acceptTermsChecked}
-                    onChange={() => setAcceptTermsChecked(!acceptTermsChecked)}
-                  />
-                  <span className=" tracking-[0.08em] leading-[120%] text-xs sm:text-[16px] flex items-center  cursor-pointer">
-                    Accept prelucrarea datelor cu caracter personal
-                  </span>
-                </div>
-              </div>
-              <div className="flex text-dark-navy">
-                <div className="flex items-center justify-center">
-                  <input
-                    type="checkbox"
-                    id="termsAndConditionsCheckbox"
-                    className="mr-2 cursor-pointer h-5 w-5"
-                    checked={termsAndConditionsChecked}
-                    onChange={() =>
-                      setTermsAndConditionsChecked(!termsAndConditionsChecked)
-                    }
-                  />
-                  <span className=" tracking-[0.08em] leading-[120%] text-xs sm:text-[16px] flex items-center cursor-pointer">
-                    Sunt de acord cu &nbsp;
-                    <span className="underline">Termenii și Conditiile</span>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="w-[20rem] mt-5 flex flex-col items-start justify-center gap-[1.5rem]">
-              <div className="self-stretch flex flex-col items-start justify-start cursor-pointer text-center text-[0.88rem] text-white">
-                <div
-                  className="self-stretch rounded bg-accent h-[2.25rem] flex flex-row items-center justify-start py-[0rem] px-[1rem] box-border "
-                  onClick={
-                    () => {
-                      termsAndConditionsChecked && acceptTermsChecked
-                        ? console.log("NEXT PAGE")
-                        : console.log("STAY ON THIS PAGE");
-                    }
-                    //IF ACCEPT TERMS AND ACCEPT PRELUCRARE -> NEXT PAGE
-                  }
+      <div className={cartPage === "1" ? "" : "hidden"}>
+        <ProgressBar cartStep={"1"} />
+        <div className="w-[wh] flex flex-col bg-white overflow-hidden text-left text-[1rem] text-dark-navy font-text-body">
+          <div className="w-[wh] max-w-[1050px] flex justify-center items-center xl:mr-[36rem] lg:mr-[22rem] md:mr-[9rem] sm:ml-[0rem] ">
+            <div className=" flex flex-col items-center sm:items-start py-[1.5rem] gap-[2.5rem]">
+              <div className="flex">
+                <button
+                  className="self-stretch flex flex-row  gap-[1rem]"
+                  onClick={() => (window.location.href = "/product")}
                 >
-                  <b className="flex-1 relative tracking-[0.15em] leading-[120%] uppercase flex items-center justify-center h-[2.25rem]">
-                    Continuă
-                  </b>
+                  <img
+                    className="relative rounded-sm w-[0.83rem] h-[1.63rem]"
+                    alt=""
+                    src={back_icon_arrow}
+                  />
+                  <div className="relative tracking-[0.05em] leading-[1.5rem] font-medium flex items-center h-[1.5rem] shrink-0">
+                    Continua cumparaturile
+                  </div>
+                </button>
+              </div>
+              <div className="flex flex-col gap-[1.5rem] text-text-fields-grey-hf w-[wh]">
+                <ProductsList
+                  handleRemoveButton={handleRemoveButton}
+                  products={products}
+                  baseUrl={baseUrl}
+                />
+
+                <div className=" tracking-[0.08em] leading-[120%] font-bold text-[1.2rem] flex gap-[0.5rem] text-dark-navy">
+                  Total de plată: {price_total} RON
+                </div>
+
+                <div className="flex text-dark-navy">
+                  <div className="flex items-center justify-center">
+                    <input
+                      type="checkbox"
+                      id="acceptTermsCheckbox"
+                      className="mr-2 cursor-pointer h-5 w-5"
+                      checked={acceptTermsChecked}
+                      onChange={() =>
+                        setAcceptTermsChecked(!acceptTermsChecked)
+                      }
+                    />
+                    <span className=" tracking-[0.08em] leading-[120%] text-xs sm:text-[16px] flex items-center  cursor-pointer">
+                      Accept prelucrarea datelor cu caracter personal
+                    </span>
+                  </div>
+                </div>
+                <div className="flex text-dark-navy">
+                  <div className="flex items-center justify-center">
+                    <input
+                      type="checkbox"
+                      id="termsAndConditionsCheckbox"
+                      className="mr-2 cursor-pointer h-5 w-5"
+                      checked={termsAndConditionsChecked}
+                      onChange={() =>
+                        setTermsAndConditionsChecked(!termsAndConditionsChecked)
+                      }
+                    />
+                    <span className=" tracking-[0.08em] leading-[120%] text-xs sm:text-[16px] flex items-center cursor-pointer">
+                      Sunt de acord cu &nbsp;
+                      <span className="underline">Termenii și Conditiile</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[20rem] mt-5 flex flex-col items-start justify-center gap-[1.5rem]">
+                <div className="self-stretch flex flex-col items-start justify-start cursor-pointer text-center text-[0.88rem] text-white">
+                  <div
+                    className="self-stretch rounded bg-accent h-[2.25rem] flex flex-row items-center justify-start py-[0rem] px-[1rem] box-border "
+                    onClick={
+                      () => {
+                        continueRequirements && products.length !== 0
+                          ? setCartPage("2")
+                          : console.log("STAY ON THIS PAGE");
+                      }
+                      //IF ACCEPT TERMS AND ACCEPT PRELUCRARE -> NEXT PAGE
+                    }
+                  >
+                    <b className="flex-1 relative tracking-[0.15em] leading-[120%] uppercase flex items-center justify-center h-[2.25rem]">
+                      Continuă
+                    </b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* //second cart page */}
+      <div className={cartPage === "2" ? "" : "hidden"}>
+        <ProgressBar cartStep={"2"} />
+        <div className="w-[wh] flex flex-col bg-white overflow-hidden text-left text-[1rem] text-dark-navy font-text-body">
+          <div className="w-[wh] max-w-[1050px] flex justify-center items-center xl:mr-[36rem] lg:mr-[22rem] md:mr-[9rem] sm:ml-[0rem] ">
+            <div className=" flex flex-col items-center sm:items-start py-[1.5rem] gap-[2.5rem]">
+              <div className="flex">
+                <button
+                  className="self-stretch flex flex-row  gap-[1rem]"
+                  onClick={() => (window.location.href = "/product")}
+                >
+                  <img
+                    className="relative rounded-sm w-[0.83rem] h-[1.63rem]"
+                    alt=""
+                    src={back_icon_arrow}
+                  />
+                  <div className="relative tracking-[0.05em] leading-[1.5rem] font-medium flex items-center h-[1.5rem] shrink-0">
+                    Continua cumparaturile
+                  </div>
+                </button>
+              </div>
+              <div className="flex flex-col gap-[1.5rem] text-text-fields-grey-hf w-[wh]">
+                <div className=" tracking-[0.08em] leading-[120%] font-bold text-[1.2rem] flex gap-[0.5rem] text-dark-navy">
+                  Total de plată: {price_total} RON
+                </div>
+
+                <div className="flex text-dark-navy">
+                  <div className="flex items-center justify-center">
+                    <input
+                      type="checkbox"
+                      id="acceptTermsCheckbox"
+                      className="mr-2 cursor-pointer h-5 w-5"
+                      checked={acceptTermsChecked}
+                      onChange={() =>
+                        setAcceptTermsChecked(!acceptTermsChecked)
+                      }
+                    />
+                    <span className=" tracking-[0.08em] leading-[120%] text-xs sm:text-[16px] flex items-center  cursor-pointer">
+                      Accept prelucrarea datelor cu caracter personal
+                    </span>
+                  </div>
+                </div>
+                <div className="flex text-dark-navy">
+                  <div className="flex items-center justify-center">
+                    <input
+                      type="checkbox"
+                      id="termsAndConditionsCheckbox"
+                      className="mr-2 cursor-pointer h-5 w-5"
+                      checked={termsAndConditionsChecked}
+                      onChange={() =>
+                        setTermsAndConditionsChecked(!termsAndConditionsChecked)
+                      }
+                    />
+                    <span className=" tracking-[0.08em] leading-[120%] text-xs sm:text-[16px] flex items-center cursor-pointer">
+                      Sunt de acord cu &nbsp;
+                      <span className="underline">Termenii și Conditiile</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[20rem] mt-5 flex flex-col items-start justify-center gap-[1.5rem]">
+                <div className="self-stretch flex flex-col items-start justify-start cursor-pointer text-center text-[0.88rem] text-white">
+                  <div
+                    className="self-stretch rounded bg-accent h-[2.25rem] flex flex-row items-center justify-start py-[0rem] px-[1rem] box-border "
+                    onClick={
+                      () => {
+                        termsAndConditionsChecked && acceptTermsChecked
+                          ? (window.location.href = "/CheckoutDetails")
+                          : console.log("STAY ON THIS PAGE");
+                      }
+                      //IF ACCEPT TERMS AND ACCEPT PRELUCRARE -> NEXT PAGE
+                    }
+                  >
+                    <b className="flex-1 relative tracking-[0.15em] leading-[120%] uppercase flex items-center justify-center h-[2.25rem]">
+                      Continuă
+                    </b>
+                  </div>
                 </div>
               </div>
             </div>
