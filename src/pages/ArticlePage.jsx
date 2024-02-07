e4fd60c41d65306020a273305463ae7a864301a9
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-02-07 15:59:10 +0100

    Article page and individual articles

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,17 +1,49 @@
+import axios_api from "../api/axios_api";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import ArticleHead from "../components/ArticleHead";
 import ArticleParagraph from "../components/ArticleParagraph";
 import ArticlesOption from "../components/ArticlesOption";
 const ArticlePage = () => {
+  const id = useParams();
+
+  const [currentArticle, setCurrentArticle] = useState();
+  const get_article = () => {
+    const url = `/get_article?id=${id.id}`;
+    try {
+      axios_api
+        .get(url, {
+          params: {
+            // for category iterate through selectCategories and choose the name with the same raw
+          },
+          withCredentials: true,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            setCurrentArticle(response.data);
+            console.log(currentArticle);
+          }
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    get_article();
+  }, []);
+
   return (
     <div className="pb-12">
       <div>
-        <ArticleHead />
+        <ArticleHead article={currentArticle} />
         <ArticlesOption topPx={1165} />
       </div>
       <div>
-        <ArticleParagraph />
-        <ArticleParagraph />
-        <ArticleParagraph />
+        {currentArticle?.texts.map((data, index) => (
+          <ArticleParagraph key={index} paragraphData={data} />
+        ))}
       </div>
     </div>
   );
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,33 +1,44 @@
+import { useState, useEffect } from "react";
+import axios_api from "../api/axios_api";
+import { ArticleBar } from "../components/ArticleBar";
 import ArticlesOption from "../components/ArticlesOption";
 import DisplayArticle from "../components/DisplayArticle";
 
 const Articles = () => {
+  const [articles, setArticles] = useState([]);
+  const get_all_articles = () => {
+    try {
+      axios_api
+        .get("/get_articles", {
+          params: {},
+          withCredentials: true,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            const json = response.data;
+            console.log(json);
+            setArticles(json);
+          }
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    get_all_articles();
+    console.log(articles);
+  }, []);
+
   return (
     <div>
-      <div className=" bg-light-purple h-12 md:h-56 flex items-center justify-center">
-        <div className="w-full flex items-center max-w-[1450px] justify-center flex-col lg:flex-row lg:justify-between md:px-28">
-          <div className="flex flex-col items-center lg:items-start mb-2">
-            <div className="flex-col mt-2 text-2xl sm:text-3xl md:text-4xl tracking-[0.05em]  font-semibold flex justify-center items-center h-10 shrink-0 ">
-              Articole
-            </div>
-            <div className="text-sm hidden mt-2 md:flex text-center lg:text-left pb-3 lg:pb-0 sm:text-lg md:text-xl tracking-[0.05em] leading-[24px] items-center max-w-[900px] shrink-0">
-              Pe măsură ce indivizii trec cu grație în anii lor de aur,
-              prioritizarea sănătății devine esențială pentru menținerea unui
-              stil de viață vibrant și împlinit. Îmbătrânirea este un proces
-              natural care aduce schimbări în bunăstarea fizică, mentală și
-              emoțională.
-            </div>
-          </div>
-        </div>
-      </div>
+      <ArticleBar />
       <div className="">
         <div>
-          {/* article.map(article) => <DisplayArticle id=article.id image=article.image 
-          title=article.title date=article.date description= article.description/>  */}
-          <DisplayArticle id={1} />
-          <DisplayArticle id={2} />
-          <DisplayArticle id={3} />
-          <DisplayArticle id={4} />
+          {articles.map((article) => (
+            <DisplayArticle article={article} />
+          ))}
         </div>
       </div>
       <ArticlesOption topPx={435} />
