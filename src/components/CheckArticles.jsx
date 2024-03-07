87741327eff86dac27b26f72fbb694acf14b244d
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-03-07 16:46:28 +0100

    Changed article bar text

--- src/components/CheckArticles.jsx
+++ src/components/CheckArticles.jsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 
 const CheckArticles = () => {
   return (
-    <div className=" bg-light-purple mt-10 mb-20 h-40 flex items-center justify-center ">
+    <div className=" bg-light-purple mt-10 mb-20 h-48 flex items-center justify-center ">
       <div className="w-full flex items-center  justify-center flex-col lg:flex-row lg:justify-between md:px-28 max-w-[1450px]">
         <div className="flex flex-col items-center lg:items-start">
           <div className="flex-col text-lg sm:text-2xl md:text-3xl tracking-[0.05em]  font-semibold flex items-center h-10 shrink-0 ">
             Descoperă articolele noastre
           </div>
-          <div className="text-sm pb-3 lg:pb-0 sm:text-lg md:text-xl tracking-[0.05em] leading-[24px] flex items-center max-w-[586px] h-10 shrink-0">
-            Lorem ipsum dolor sit amet, qui minim labore
+          <div className="text-sm pb-3 lg:pb-0 sm:text-lg md:text-xl tracking-[0.025em] leading-[24px] flex items-center max-w-[586px] shrink-0">
+            Explorează articolele noastre acum și descoperă modalități de a trăi o viață activă și împlinită la orice vârstă!
           </div>
         </div>
         <NavLink to="/articles">
